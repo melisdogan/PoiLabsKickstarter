9e625c33cc4f4ef43ac21380c07d3e1df1e36548
melis <melis@melisdogan.com>
2020-08-01 15:24:57 +0300

    init

---
+++ app/src/main/java/com/example/poilabskickstarter/ui/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.poilabskickstarter.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.poilabskickstarter.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
